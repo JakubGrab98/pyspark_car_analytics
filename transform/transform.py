a9a9625c00e1da8c06b04b0fb87c3243fd27cebf
Jakub Grabarczyk <kubagrabarczyk98@gmail.com>
2024-11-08 20:46:19 +0100

    added clean data method with initial transformation

--- transform/transform.py
+++ transform/transform.py
@@ -1,14 +1,41 @@
-from pyspark.sql import SparkSession
+from pyspark.sql import SparkSession, DataFrame
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
 
 
 class CarsTransformation:
+
+    COLUMNS_LIST = [
+        "id", "url", "region", "region_url", "price",
+        "year", "manufacturer", "model", "condition",
+        "fuel", "odometer", "transmission", "VIN",
+        "drive", "size", "type", "paint_color",
+        "descritpion", "state", "lat", "long", "posting_date",
+    ]
+
     def __init__(self, spark_session: SparkSession):
         self.spark_session = spark_session
 
     def load_data(self, csv_file: str):
         df = (
-            spark.read
+            self.spark_session.read
             .option("header", "true")
             .csv(csv_file)
         )
         return df
+
+    def clean_data(self, df: DataFrame) -> DataFrame:
+        clean_df = (
+            df
+            .withColumn("price", col("price").cast(IntegerType()))
+            .withColumn("odometer", col("odometer").cast(LongType()))
+            .withColumn("lat", col("lat").cast(FloatType()))
+            .withColumn("long", col("long").cast(FloatType()))
+            .filter(
+                col("model").isNotNull()
+                & col("manufacturer").isNotNull()
+                & col("price") > 0
+            )
+            .select(self.COLUMNS_LIST)
+        )
+        return clean_df
