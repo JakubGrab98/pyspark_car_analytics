b27013e913a682baf4bc4f90cf013a30e483d705
Jakub Grabarczyk <kubagrabarczyk98@gmail.com>
2024-11-14 20:55:19 +0100

    moved column names variables to const module

--- analytics/cars_filter.py
+++ analytics/cars_filter.py
@@ -1,12 +1,14 @@
 from pyspark.sql import DataFrame
 from pyspark.sql.functions import *
+from const import (
+    PRODUCER_COLUMN,
+    MODEL_COLUMN,
+    YEAR_COLUMN,
+    PRICE_COLUMN,
+)
+
 
 class CarsFilter:
-
-    PRODUCER_COLUMN: str = "producer"
-    MODEL_COLUMN: str = "model"
-    YEAR_COLUMN: str = "year"
-    PRICE_COLUMN: str = "price_PLN"
 
     def __init__(
             self, producer: str, model: str, min_prod_year: int = 1900,
@@ -20,24 +22,24 @@ class CarsFilter:
         self.max_price = max_price
 
     def filter_by_producer(self, df: DataFrame) -> DataFrame:
-        return df.filter(col(self.PRODUCER_COLUMN) == self.producer)
+        return df.filter(col(PRODUCER_COLUMN) == self.producer)
 
     def filter_by_model(self, df: DataFrame)-> DataFrame:
         return df.filter(
-            (col(self.PRODUCER_COLUMN) == self.PRODUCER_COLUMN)
-            & (col(self.MODEL_COLUMN) == self.model)
+            (col(PRODUCER_COLUMN) == self.PRODUCER_COLUMN)
+            & (col(MODEL_COLUMN) == self.model)
         )
 
     def filter_by_price(self, df: DataFrame) -> DataFrame:
         filtered_df = df.filter(
-            (col(self.PRICE_COLUMN) >= self.min_price) &
-            (self.max_price <= 0 | (col(self.PRICE_COLUMN) <= self.max_price))
+            (col(PRICE_COLUMN) >= self.min_price) &
+            (self.max_price <= 0 | (col(PRICE_COLUMN) <= self.max_price))
         )
         return filtered_df
 
     def filter_by_year(self, df: DataFrame) -> DataFrame:
         filtered_df = df.filter(
-            (col(self.YEAR_COLUMN) >= self.min_prod_year) &
-            (col(self.YEAR_COLUMN) <= self.min_prod_year)
+            (col(YEAR_COLUMN) >= self.min_prod_year) &
+            (col(YEAR_COLUMN) <= self.min_prod_year)
         )
         return filtered_df
