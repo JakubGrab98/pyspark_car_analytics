fcefa8ea18ae44d2e27b03926ab3a5c291b55253
Jakub Grabarczyk <kubagrabarczyk98@gmail.com>
2024-11-14 21:33:01 +0100

    updated main module

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from pyspark.sql import SparkSession
 from transform.transform import clean_data, get_price_in_pln, save_data_to_parquet
+from analytics.cars_filter import CarsFilter
+from analytics.cars_reports import CarReport
 from rates.nbp_rates import Rates
 from const import NBP_API
 
@@ -11,12 +13,22 @@ def transformation(spark):
     raw_cars_df = (
         spark.read
         .option("header", "true")
-        .csv("data/raw/cars_data.csv")
+        .csv("data/raw/otomoto_data.csv")
     )
 
     cleaned_car_df = raw_cars_df.transform(clean_data)
     updated_price_df = get_price_in_pln(cleaned_car_df, rates_df)
+    updated_price_df.show()
+    updated_price_df.printSchema()
     save_data_to_parquet(updated_price_df, ["year", "producer"], "data/transform")
+
+def reporting(spark: SparkSession):
+    cleaned_df = spark.read.parquet("data/transform")
+    car_filter = CarsFilter("Audi", "A4")
+    car_reports = CarReport(cleaned_df, car_filter)
+    car_reports.get_model_statistics().show()
+    print(car_reports.get_number_of_producer_advertises())
+
 
 def main():
     spark = (SparkSession.builder
@@ -24,7 +36,8 @@ def main():
             .master("local")
             .getOrCreate()
     )
-    transformation(spark)
+    # transformation(spark)
+    reporting(spark)
 
 
 if __name__ == "__main__":
