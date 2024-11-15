bd0a6de441809d75eb5b45818cbc2eabc0905570
Jakub Grabarczyk <kubagrabarczyk98@gmail.com>
2024-11-15 07:38:38 +0100

    added get_advertises method and added column mapping

--- const.py
+++ const.py
@@ -3,3 +3,17 @@ PRODUCER_COLUMN: str = "producer"
 MODEL_COLUMN: str = "model"
 YEAR_COLUMN: str = "year"
 PRICE_COLUMN: str = "price_PLN"
+URL_COLUMN: str = "url"
+MILEAGE_COLUMN: str = "mileage"
+MILEAGE_UNIT_COLUMN: str = "mileage_unit"
+VERSION_COLUMN: str = "version"
+
+REPORT_COLUMNS_MAPPING = {
+    PRODUCER_COLUMN: "Producer",
+    MODEL_COLUMN: "Model",
+    YEAR_COLUMN: "Production Year",
+    PRICE_COLUMN: "Price",
+    URL_COLUMN: "OtoMoto URL",
+    MILEAGE_COLUMN: "Mileage",
+    VERSION_COLUMN: "Version",
+}
