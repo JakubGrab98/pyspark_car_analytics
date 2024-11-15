01c2819698fa3d469a07d36d76fdd437e39d4d4b
Jakub Grabarczyk <kubagrabarczyk98@gmail.com>
2024-11-15 07:04:04 +0100

    removed method for average model price calculation

--- analytics/cars_reports.py
+++ analytics/cars_reports.py
@@ -16,23 +16,13 @@ class CarReport:
 
     def get_number_of_producer_advertises(self) -> int:
         base_df = self.df.select(PRODUCER_COLUMN)
-        result = self.cars_filter.filter_by_producer(base_df).count()
+        result = self.cars_filter.filter_by_all_parameters(base_df).count()
         return result
-
-    def get_average_price_by_model(self) -> DataFrame:
-        result_df = (
-            self.df.select(PRODUCER_COLUMN, MODEL_COLUMN, PRICE_COLUMN)
-            .transform(self.cars_filter.filter_by_model)
-            .groupby(PRODUCER_COLUMN, MODEL_COLUMN)
-            .avg(PRICE_COLUMN)
-            .alias("average_price")
-        )
-        return result_df
 
     def get_model_statistics(self) -> DataFrame:
         statistics_df = (
             self.df.select(PRODUCER_COLUMN, MODEL_COLUMN, YEAR_COLUMN, PRICE_COLUMN)
-            .transform(self.cars_filter.filter_by_model)
+            .transform(self.cars_filter.filter_by_all_parameters)
             .groupby(PRODUCER_COLUMN, MODEL_COLUMN)
             .agg(
                 max(YEAR_COLUMN).alias("max_prod_year"),
