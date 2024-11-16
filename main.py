6bfadfd54d9f13473c2a4c458bf33da3f64706d5
Jakub Grabarczyk <kubagrabarczyk98@gmail.com>
2024-11-16 12:32:41 +0100

    created initial interface

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import streamlit as st
+from pygments.lexer import default
 from pyspark.sql import SparkSession
 from transform.transform import clean_data, get_price_in_pln, save_data_to_parquet
 from analytics.cars_filter import CarsFilter
@@ -36,9 +38,32 @@ def main():
             .master("local")
             .getOrCreate()
     )
-    # transformation(spark)
-    reporting(spark)
+    transformation(spark)
+    transformed_df = spark.read.parquet("data/transform")
+    return transformed_df
 
 
 if __name__ == "__main__":
-    main()
+    base_df = main().cache()
+    st.title("Cars Advertisement Analysis")
+
+    with st.sidebar:
+        producer = st.text_input("Enter producer name:")
+        model = st.text_input("Enter model name:")
+        min_year = st.number_input("Minimum production year:", value = 1900, min_value=1900, max_value=2100)
+        max_year = st.number_input("Maximum production year:", value = 2999, min_value=1900, max_value=2999)
+        min_price = st.number_input("Minimum price:", value=0, min_value=0)
+        max_price = st.number_input("Maximum price:", value=0, min_value=0)
+
+
+    if st.button("Start Analysis"):
+        if producer and model:
+            with st.spinner("Analyzing data..."):
+                car_filter = CarsFilter(producer, model, min_year, max_year, min_price, max_price)
+                car_reports = CarReport(base_df, car_filter)
+                st.markdown("## Car Analysis")
+                st.table(car_reports.get_model_statistics())
+                st.table(car_reports.get_advertises())
+                st.table(car_reports.compare_other_producers())
+        else:
+            st.error("Please fill in all fields.")
